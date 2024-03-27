73100fb93cf5e9312347160ac7ddbe74d05378fe
Colm Woods <100407192+colmwoods@users.noreply.github.com>
2024-03-27 18:40:32 +0000

    Update snipper.py

--- snipper.py
+++ snipper.py
@@ -9,7 +9,7 @@ def main(name, url):
     catch = rf"^https?://(?:www\.)?{re.escape(name)}.com/(?:[a-zA-Z0-9.-]+/)?/?(\d+)?/?@?([a-zA-Z0-9_.-]+)" # How Its Going Snip Your Name
     
     if matches := re.search(catch, url, re.IGNORECASE):
-        print(f"This Accounts Username:", matches.group(2)) # matches.group(2) Is Giving Us The Second Capturing Group
+        print(f"This {name} Accounts Username:", matches.group(2)) # matches.group(2) Is Giving Us The Second Capturing Group
         
     else:
         print("Incorrect Application Or URL")
